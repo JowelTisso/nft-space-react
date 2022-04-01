b4fed43fae90a65b57946e46a3ed8dc4f815e912
Jowel Tisso <52632590+JowelTisso@users.noreply.github.com>
2022-04-01 14:00:44 +0530

    feat: wishlist auth feature added

--- src/context/reducer/wishlist/wishlistReducerHelper.js
+++ src/context/reducer/wishlist/wishlistReducerHelper.js
@@ -1,39 +1,19 @@
-import { Action } from "history";
 import {
-  ADD_TO_WISHLIST,
   GET_WISHLIST_DATA,
-  REMOVE_FROM_WISHLIST,
+  UPDATE_WISHLIST_DATA,
 } from "../../../utils/Constant";
-
-const addToWishlist = (state, action) => {
-  const itemIndex = state.wishlistItems.findIndex(
-    (item) => item._id === action.payload._id
-  );
-  if (itemIndex < 0) {
-    return [...state.wishlistItems].concat(action.payload);
-  }
-  return [...state.wishlistItems];
-};
-
-const removeFromWishlist = (state, action) =>
-  [...state.wishlistItems].filter((item) => item._id != action.payload._id);
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case ADD_TO_WISHLIST:
-      return {
-        ...state,
-        wishlistItems: addToWishlist(state, action),
-      };
-    case REMOVE_FROM_WISHLIST:
-      return {
-        ...state,
-        wishlistItems: removeFromWishlist(state, action),
-      };
     case GET_WISHLIST_DATA:
       return {
         ...state,
-        wishlistItems: action.payload,
+        wishlistItems: [...action.payload],
+      };
+    case UPDATE_WISHLIST_DATA:
+      return {
+        ...state,
+        wishlistItems: [...action.payload],
       };
     default:
       return state;
--- src/pages/products/helper/WishlistHelper.js
+++ src/pages/products/helper/WishlistHelper.js
@@ -1,15 +1,50 @@
-import { ADD_TO_WISHLIST, REMOVE_FROM_WISHLIST } from "../../../utils/Constant";
+import axios from "axios";
+import { UPDATE_WISHLIST_DATA } from "../../../utils/Constant";
+import { getUserToken } from "../../../utils/TokenHelper";
 
-export const addToWishlist = (item, dispatch) => {
-  dispatch({
-    type: ADD_TO_WISHLIST,
-    payload: item,
-  });
+export const addToWishlist = async (item, dispatch) => {
+  try {
+    const token = getUserToken();
+    if (token) {
+      const { status, data } = await axios.post(
+        "/api/user/wishlist",
+        {
+          product: item,
+        },
+        {
+          headers: { authorization: token },
+        }
+      );
+      if (status === 200 || 201) {
+        dispatch({
+          type: UPDATE_WISHLIST_DATA,
+          payload: data?.wishlist,
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const removeFromWishlist = (item, dispatch) => {
-  dispatch({
-    type: REMOVE_FROM_WISHLIST,
-    payload: item,
-  });
+export const removeFromWishlist = async (item, dispatch) => {
+  try {
+    const token = getUserToken();
+    if (token) {
+      const { status, data } = await axios.delete(
+        `/api/user/wishlist/${item._id}`,
+        {
+          headers: { authorization: token },
+        }
+      );
+      if (status === 200 || 201) {
+        dispatch({
+          type: UPDATE_WISHLIST_DATA,
+          payload: data?.wishlist,
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
